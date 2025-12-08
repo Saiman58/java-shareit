e28741ea6129cacbcc30c765a446e6616630f9e4
Alex <radist.al@yandex.ru>
2025-12-08 22:59:28 +0300

    ФЗ-16 (part 1)

--- src/main/java/ru/practicum/shareit/request/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestService.java
@@ -5,13 +5,13 @@ import java.util.List;
 public interface ItemRequestService {
 
     //создать запрос
-    ItemRequestResponseDto createRequest (Long userId, ItemRequestDto itemRequestDto);
+    ItemRequestResponseDto createRequest(Long userId, ItemRequestDto itemRequestDto);
 
     //получить все запросы текущего пользователя
-    List<ItemRequestResponseDto> getUserRequests (Long userId);
+    List<ItemRequestResponseDto> getUserRequests(Long userId);
 
     //получить запросы других пользователей
-    List<ItemRequestResponseDto> getAllRequests (Long userId, Integer from,Integer size);
+    List<ItemRequestResponseDto> getAllRequests(Long userId, Integer from, Integer size);
 
     //получить запрос по ID
     ItemRequestResponseDto getRequestById(Long userId, Long requestId);
